cbb206ed540b7789de34845a56568a96e047be5a
hugo0190. <hugo_gramme@estudante.sesisenai.org.br>
2026-07-02 09:31:06 -0300

    add- atividade 5 feita

---
+++ ex_05.php
@@ -0,0 +1,28 @@
+<?php
+
+function analisarTexto($texto)
+{
+    $palavras = str_word_count($texto);
+    $caracteres = strlen($texto);
+
+    $vogais = preg_match_all("/[aeiouAEIOU]/", $texto);
+
+    $consoantes = preg_match_all("/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/", $texto);
+
+    return [
+        "palavras" => $palavras,
+        "caracteres" => $caracteres,
+        "vogais" => $vogais,
+        "consoantes" => $consoantes
+    ];
+}
+
+$texto = "Programar em PHP e muito legal";
+
+$resultado = analisarTexto($texto);
+
+echo "Texto: " . $texto . "<br><br>";
+echo "Quantidade de palavras: " . $resultado["palavras"] . "<br>";
+echo "Quantidade de caracteres: " . $resultado["caracteres"] . "<br>";
+echo "Quantidade de vogais: " . $resultado["vogais"] . "<br>";
+echo "Quantidade de consoantes: " . $resultado["consoantes"];
